61f947f74a896fe26e72e9bb8f989fd3735f7491
Cguy8 <48178497+Cguy8@users.noreply.github.com>
2019-03-03 08:57:57 -0800

    Location

--- driver.py
+++ driver.py
@@ -1,3 +1,5 @@
+from location import *
+
 def userinput(string):
     IP = ""
     while(not(IP)):
@@ -31,21 +33,52 @@ def DriverScript():
     ID = ""
     route = ""
     valid = False
-    while(word != "leave"):
-        while(not(valid)):
-            word = userinput("Please login with your ID: ")
-            if(word == "leave"):
-              break
-            else:
-                word = "{0:0>6}".format(int(word))
-                valid, line = checkvalid(word)
+    while(not(valid)):
+        word = userinput("Please login with your ID: ")
+        if(word == "leave"):
+            break
+        else:
+            word = "{0:0>6}".format(int(word))
+            valid, line = checkvalid(word)
         ID, route = line.split(" ")
         print("")
         print("Your ID is: " + ID)
         print("Your Route# is: "+ route)
 
-        word = userinput("What do you want to do? ")
+    while(word != "leave"):
+        word = userinput("What do you want to do? \n[1] Check in student\n[2] List students\n[3] Get location\n[0] Logout\n")
+        if(word == '1'):
+            stuID = input("Enter the student's ID number: ")
+            infile = open("Buses.txt", "r")
+            busVect = []
+            for line in infile:
+                busVect.append(line[:-1])
+
+     #       for i in range (len(busVect)):
+      #          print(busVect[i])
                 
+            infile.close
+            for i in range(len(busVect)-1):
+                if(busVect[i].find(str(route))):
+                   stuVect = str(busVect[i])
+                   busVect.pop(i)
+            print(stuVect)
+            busVect.append(stuVect + " " + stuID)
+            outfile = open("Buses.txt", "w")
+            for i in range (len(busVect)):
+                line = busVect[i]
+                print(line, file=outfile)
+            outfile.close()
+        if(word == '2')
+            print("This should print the students on the bus")
+        if(word == '3')
+            a = input("Input a latitide: ")
+            b = input("Input a longitude: ")
+            a = float(a)
+            b = float(b)
+            GeoScript(a,b)
+        if(word == '0')
+            break
 
 
 def main():
