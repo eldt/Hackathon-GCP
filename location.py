b3cf5bfcab71dfa3a96d6383dd7b2a622f408608
Sauceious <48178507+Sauceious@users.noreply.github.com>
2019-03-03 08:25:25 -0800

    Update location.py

--- location.py
+++ location.py
@@ -3,13 +3,12 @@ from datetime import datetime
 import googlemaps
 
 
-def GeoScript(latvar, longvar):
+def GeoScript(float(latvar), float(longvar)):
     gmaps = googlemaps.Client(key= 'AIzaSyD98Jrf_CWeR-tEug615QX_rL6Y_qXjUsw')
     
     geo_code = gmaps.reverse_geocode((latvar, longvar))
 
     print(geo_code)
-    
-A = input("latitude = ")
-B = input("longitude= ")
-GeoScript(float(A),float(B))
+A = input("latitude = ") 
+B = input("longitude = ")
+GeoScript(A, B)
